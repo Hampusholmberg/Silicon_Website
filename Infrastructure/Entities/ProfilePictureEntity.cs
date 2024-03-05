da80582a48203f9baffdfc223cff5a449b93afbd
Hampusholmberg <117671157+Hampusholmberg@users.noreply.github.com>
2024-03-05 16:09:34 +0100

    ..

--- Infrastructure/Entities/ProfilePictureEntity.cs
+++ Infrastructure/Entities/ProfilePictureEntity.cs
@@ -3,6 +3,6 @@
 public class ProfilePictureEntity
 {
     public int Id { get; set; }
-    public string ImageUrl { get; set; } = null!;
+    public string? ImageUrl { get; set; }
     public string? AltText { get; set; }
 }
